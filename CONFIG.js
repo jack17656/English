77cd585f0abb9030a781d34b1db44fee4d096337
jack17656 <114502766+jack17656@users.noreply.github.com>
2022-09-28 20:18:31 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -5,4 +5,6 @@ const CONFIGDATA = {
   buttonYes: "Absolutly",
   buttonNo: "No",
   titleModar: "Ok then lets begin now",
-  
+  descriptionModar:
+    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+};
